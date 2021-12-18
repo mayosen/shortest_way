654737bdcd336cdce7e416de2d06a8fc3000caa2
Yegor <gare577@gmail.com>
2021-12-18 18:38:36 +0300

    Improved saving data

--- file_write.m
+++ file_write.m
@@ -6,10 +6,7 @@ fprintf(file, "%s\n", datestr(takes, "HH:MM"));
 fprintf(file, "%.3f\n", cost);
 fprintf(file, "%i ", opt);
 fprintf(file, "\n");
-
-for msg = messages
-    fprintf(file, "%s", msg);
-end
+fprintf(file, "%s", messages);
 
 fclose(file);
 
